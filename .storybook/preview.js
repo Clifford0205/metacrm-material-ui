d8fba2acb3963eb29ac40980a18cd81605915be6
clifford <ts02690140@gmail.com>
2023-06-21 13:58:20 +0800

    feat(eslint): lint

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -7,52 +7,52 @@ import ColorModeContext from '@/contexts/ColorMode.context';
 import useColorMode from '@/hooks/useColorMode.hooks';
 
 const preview = {
-  parameters: {
-    actions: { argTypesRegex: '^on[A-Z].*' },
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/,
-      },
-    },
-  },
-  globalTypes: {
-    theme: {
-      name: 'Theme',
-      title: 'Theme',
-      description: 'Theme for your components',
-      defaultValue: 'light',
-      toolbar: {
-        icon: 'paintbrush',
-        dynamicTitle: true,
-        items: [
-          { value: 'light', left: '☀️', title: 'Light mode' },
-          { value: 'dark', left: '🌙', title: 'Dark mode' },
-        ],
-      },
-    },
-  },
-  decorators: [
-    (Story, context) => {
-      const { colorModeHooksValue, theme } = useColorMode();
-      const { toggleColorMode, mode } = colorModeHooksValue;
+	parameters: {
+		actions: { argTypesRegex: '^on[A-Z].*' },
+		controls: {
+			matchers: {
+				color: /(background|color)$/i,
+				date: /Date$/,
+			},
+		},
+	},
+	globalTypes: {
+		theme: {
+			name: 'Theme',
+			title: 'Theme',
+			description: 'Theme for your components',
+			defaultValue: 'light',
+			toolbar: {
+				icon: 'paintbrush',
+				dynamicTitle: true,
+				items: [
+					{ value: 'light', left: '☀️', title: 'Light mode' },
+					{ value: 'dark', left: '🌙', title: 'Dark mode' },
+				],
+			},
+		},
+	},
+	decorators: [
+		(Story, context) => {
+			const { colorModeHooksValue, theme } = useColorMode();
+			const { toggleColorMode, mode } = colorModeHooksValue;
 
-      const { theme: themeKey } = context.globals;
+			const { theme: themeKey } = context.globals;
 
-      useEffect(() => {
-        toggleColorMode(themeKey);
-      }, [themeKey, toggleColorMode]);
+			useEffect(() => {
+				toggleColorMode(themeKey);
+			}, [themeKey, toggleColorMode]);
 
-      return (
-        <ColorModeContext.Provider value={colorModeHooksValue}>
-          <ThemeProvider theme={theme}>
-            <CssBaseline />
-            <Story />
-          </ThemeProvider>
-        </ColorModeContext.Provider>
-      );
-    },
-  ],
+			return (
+				<ColorModeContext.Provider value={colorModeHooksValue}>
+					<ThemeProvider theme={theme}>
+						<CssBaseline />
+						<Story />
+					</ThemeProvider>
+				</ColorModeContext.Provider>
+			);
+		},
+	],
 };
 
 export default preview;
--- src/stories/Colors.stories.js
+++ src/stories/Colors.stories.js
@@ -12,178 +12,169 @@ import { tokens } from '@/theme/basicColors';
 import { ToastContainer, toast } from 'react-toastify';
 
 const StyledSection = styled('div', {
-  shouldForwardProp: isStyledPropsValid,
+	shouldForwardProp: isStyledPropsValid,
 })({
-  borderBottom: '1px solid #e9e9e9',
+	borderBottom: '1px solid #e9e9e9',
 });
 
 const StyledColorArea = styled('div', {
-  shouldForwardProp: isStyledPropsValid,
+	shouldForwardProp: isStyledPropsValid,
 })({
-  display: 'flex',
-  flexDirection: 'column',
-  width: '10%',
-  alignItems: 'center',
-  margin: '0 1rem 1rem 0',
+	display: 'flex',
+	flexDirection: 'column',
+	width: '10%',
+	alignItems: 'center',
+	margin: '0 1rem 1rem 0',
 
-  '&:hover': {
-    cursor: 'pointer',
-  },
+	'&:hover': {
+		cursor: 'pointer',
+	},
 
-  '& .colorKey': {
-    fontWeight: 'Bold',
-  },
+	'& .colorKey': {
+		fontWeight: 'Bold',
+	},
 
-  '& .colorCode': {
-    fontSize: '12px',
-  },
+	'& .colorCode': {
+		fontSize: '12px',
+	},
 });
 
 const StyledColorBox = styled('div', {
-  shouldForwardProp: isStyledPropsValid,
+	shouldForwardProp: isStyledPropsValid,
 })(({ theme, backgroundColor }) => ({
-  backgroundColor: backgroundColor,
-  width: '100px',
-  height: '100px',
-  borderRadius: '4px',
-  marginBottom: '5px',
-  boxShadow: '0 8px 16px 0 rgb(0 0 0 / 10%)',
+	backgroundColor: backgroundColor,
+	width: '100px',
+	height: '100px',
+	borderRadius: '4px',
+	marginBottom: '5px',
+	boxShadow: '0 8px 16px 0 rgb(0 0 0 / 10%)',
 
-  '&:hover': {
-    boxShadow: '0 8px 16px 0 rgb(0 0 0 / 40%)',
-  },
+	'&:hover': {
+		boxShadow: '0 8px 16px 0 rgb(0 0 0 / 40%)',
+	},
 }));
 
 const StyledExampleContainer = styled('div', {
-  shouldForwardProp: isStyledPropsValid,
+	shouldForwardProp: isStyledPropsValid,
 })({
-  display: 'flex',
-  flexDirection: 'column',
+	display: 'flex',
+	flexDirection: 'column',
 });
 
 const oneLevelColorList = ['black', 'white'];
 
 const ColorsTemplate = () => {
-  const { mode } = useContext(ColorModeContext);
-  const colorGuide = tokens(mode);
+	const { mode } = useContext(ColorModeContext);
+	const colorGuide = tokens(mode);
 
-  const handleCopied = (text, result) => {
-    if (result) {
-      toast.info(`Copied ${text}`);
-    }
-  };
+	const handleCopied = (text, result) => {
+		if (result) {
+			toast.info(`Copied ${text}`);
+		}
+	};
 
-  // keyName 是為了讓顏色前面有名字
-  const renderObject = targetObj => {
-    return Object.entries(targetObj).map(([key, value]) => {
-      if (typeof value === 'object') {
-        return (
-          <StyledSection key={key}>
-            <h2>{key}</h2>
-            <Box
-              display={
-                typeof Object.values(value)[0] === 'object' ? 'block' : 'flex'
-              }
-            >
-              {renderObject(value)}
-            </Box>
-          </StyledSection>
-        );
-      } else {
-        if (oneLevelColorList.includes(key)) {
-          return (
-            <StyledSection key={key}>
-              <h2>{key}</h2>
-              <CopyToClipboard text={value} onCopy={handleCopied}>
-                <StyledColorArea>
-                  <StyledColorBox backgroundColor={value} />
-                  <div className="colorCode">{value}</div>
-                </StyledColorArea>
-              </CopyToClipboard>
-            </StyledSection>
-          );
-        } else {
-          return (
-            <CopyToClipboard key={key} text={value} onCopy={handleCopied}>
-              <StyledColorArea>
-                <StyledColorBox backgroundColor={value} />
-                <div className="colorKey">{key}</div>
-                <div className="colorCode">{value}</div>
-              </StyledColorArea>
-            </CopyToClipboard>
-          );
-        }
-      }
-    });
-  };
+	// keyName 是為了讓顏色前面有名字
+	const renderObject = (targetObj) => {
+		return Object.entries(targetObj).map(([key, value]) => {
+			if (typeof value === 'object') {
+				return (
+					<StyledSection key={key}>
+						<h2>{key}</h2>
+						<Box display={typeof Object.values(value)[0] === 'object' ? 'block' : 'flex'}>
+							{renderObject(value)}
+						</Box>
+					</StyledSection>
+				);
+			} else {
+				if (oneLevelColorList.includes(key)) {
+					return (
+						<StyledSection key={key}>
+							<h2>{key}</h2>
+							<CopyToClipboard text={value} onCopy={handleCopied}>
+								<StyledColorArea>
+									<StyledColorBox backgroundColor={value} />
+									<div className='colorCode'>{value}</div>
+								</StyledColorArea>
+							</CopyToClipboard>
+						</StyledSection>
+					);
+				} else {
+					return (
+						<CopyToClipboard key={key} text={value} onCopy={handleCopied}>
+							<StyledColorArea>
+								<StyledColorBox backgroundColor={value} />
+								<div className='colorKey'>{key}</div>
+								<div className='colorCode'>{value}</div>
+							</StyledColorArea>
+						</CopyToClipboard>
+					);
+				}
+			}
+		});
+	};
 
-  return (
-    <div>
-      <h2>Colors</h2>
-      <p>Click palette to copy the color code.</p>
-      {renderObject(colorGuide)}
-      <StyledExampleContainer>
-        <h2>使用範例</h2>
+	return (
+		<div>
+			<h2>Colors</h2>
+			<p>Click palette to copy the color code.</p>
+			{renderObject(colorGuide)}
+			<StyledExampleContainer>
+				<h2>使用範例</h2>
 
-        <Box
-          width="500px"
-          p={5}
-          mb={3}
-          bgcolor={theme => theme.customColors.pink[500]}
-          color={theme => theme.customColors.white}
-        >
-          bgcolor={`{theme => theme.customColors.pink[500]}`}
-          <br />
-          color={`{theme => theme.customColors.white}`}
-          <br />
-          建議寫法
-        </Box>
+				<Box
+					width='500px'
+					p={5}
+					mb={3}
+					bgcolor={(theme) => theme.customColors.pink[500]}
+					color={(theme) => theme.customColors.white}
+				>
+					bgcolor={`{theme => theme.customColors.pink[500]}`}
+					<br />
+					color={`{theme => theme.customColors.white}`}
+					<br />
+					建議寫法
+				</Box>
 
-        <Box
-          width="500px"
-          p={5}
-          mb={3}
-          bgcolor={theme => theme.palette.primary.main}
-          color={theme => theme.palette.neutral.main}
-        >
-          bgcolor={`{theme => theme.palette.primary.main}`}
-          <br />
-          color={`{theme => theme.palette.neutral.main}`}
-        </Box>
+				<Box
+					width='500px'
+					p={5}
+					mb={3}
+					bgcolor={(theme) => theme.palette.primary.main}
+					color={(theme) => theme.palette.neutral.main}
+				>
+					bgcolor={`{theme => theme.palette.primary.main}`}
+					<br />
+					color={`{theme => theme.palette.neutral.main}`}
+				</Box>
 
-        <Box
-          width="500px"
-          p={5}
-          mb={3}
-          sx={{ bgcolor: 'secondary.main', color: 'neutral.main' }}
-        >
-          sx={`{bgcolor: 'secondary.main, color: 'neutral.main''}`}
-        </Box>
+				<Box width='500px' p={5} mb={3} sx={{ bgcolor: 'secondary.main', color: 'neutral.main' }}>
+					sx={`{bgcolor: 'secondary.main, color: 'neutral.main''}`}
+				</Box>
 
-        <Box>
-          <Button variant="contained" color="primary">
-            test
-          </Button>
-        </Box>
-      </StyledExampleContainer>
+				<Box>
+					<Button variant='contained' color='primary'>
+						test
+					</Button>
+				</Box>
+			</StyledExampleContainer>
 
-      <ToastContainer
-        position="top-center"
-        autoClose={3000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-      />
-    </div>
-  );
+			<ToastContainer
+				position='top-center'
+				autoClose={3000}
+				hideProgressBar={false}
+				newestOnTop={false}
+				closeOnClick
+				rtl={false}
+				pauseOnFocusLoss
+				draggable
+				pauseOnHover
+			/>
+		</div>
+	);
 };
 
 export const colors = ColorsTemplate.bind({});
 
 export default {
-  title: 'Example/Colors/Colors',
+	title: 'Example/Colors/Colors',
 };
